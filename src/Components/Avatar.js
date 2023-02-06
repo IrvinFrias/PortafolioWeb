b14e49ea15f1a97c6e3bd944832af7b203e0e264
Irvin Fernando Frias Torres <irvin.frias.unam@outlook.com>
2023-02-06 16:52:10 -0600

    Img portfolio changed

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,4 +1,4 @@
-import fotoPortafolio from '../assets/img/Foto-Portafolio.JPG';
+import fotoPortafolio from '../assets/img/Foto-Portafolio.png';
 const Avatar = () => {
     return(
         <img className="w-100 mx-auto border rounded-circle" src={fotoPortafolio} alt="Foto de mi portafolio"/>
